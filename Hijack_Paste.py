97573821acf1c8f34bcb6c225b3a16c71dfc6b2f
HanumaKondepi <97748393+HanumaKondepi@users.noreply.github.com>
2024-08-13 21:43:02 +0530

    Update Hijack_Paste.py
    Added function to fix copying read nodes in to a new script

--- Hijack_Paste.py
+++ Hijack_Paste.py
@@ -11,6 +11,7 @@ class HijackPaste():
         self.names = re.findall(r'name (.+)', self.clipboard_data)  # find names for all the nodes
         self.read_data = re.findall(r'Read \{[^}]*\}', self.clipboard_data)  # find data of read nodes
         self.pos_data = re.findall(r'PostageStamp \{[^}]*\}', self.clipboard_data)  # find data of postagestamps
+        self.file_paths = re.findall(r'file (.+)', self.clipboard_data)
 
         # create empty list 
         self.read_names = []
@@ -27,18 +28,23 @@ class HijackPaste():
             self.pos_names.append(name[0])
 
     def hijackpaste(self):
-        #for read_node in self.read_data:
-           # if nuke.toNode(read_node) != None
         x = self.get_pos()[0]
         y = self.get_pos()[1]
-        self.create_postage(x, y)
-        # Regular expression to find and remove blocks starting with Read and ending with }
-        read = re.sub(r'Read \{[^}]*\}', '', self.clipboard_data, flags=re.DOTALL)
-        read_nodesremoved = read.strip()  # delete all read nodes information from clipboard data
-        post = re.sub(r'PostageStamp \{[^}]*\}', '', read_nodesremoved, flags=re.DOTALL)
-        post_nodesremoved = post.strip()
-        self.clipboard.setText(post_nodesremoved)  # prep modified clipboard data
-        nuke.nodePaste('%clipboard%')
+        if self.check_path():
+            self.create_postage(x, y)
+            # Regular expression to find and remove blocks starting with Read and ending with }
+            read = re.sub(r'Read \{[^}]*\}', '', self.clipboard_data, flags=re.DOTALL)
+            read_nodesremoved = read.strip()  # delete all read nodes information from clipboard data
+            post = re.sub(r'PostageStamp \{[^}]*\}', '', read_nodesremoved, flags=re.DOTALL)
+            post_nodesremoved = post.strip()
+            self.clipboard.setText(post_nodesremoved)  # prep modified clipboard data
+            nuke.nodePaste('%clipboard%')
+        else:
+            post = re.sub(r'PostageStamp \{[^}]*\}', '', self.clipboard_data, flags=re.DOTALL)
+            post_nodesremoved = post.strip()
+            self.clipboard.setText(post_nodesremoved)  # prep modified clipboard data
+            nuke.nodePaste('%clipboard%')
+            
 
     def create_postage(self, x, y):
         files = []
@@ -69,6 +75,21 @@ class HijackPaste():
         nuke.delete(temp_node)
         return x, y
 
+    def check_path(self):
+        read_count = 0
+        for f in range(len(self.read_data)):
+            rn = re.findall(r'name (.+)', self.read_data[f])
+            rf = re.findall(r'file (.+)', self.read_data[f])
+            rns = rn[0].strip('"')
+            rfs = rf[0].strip('"')
+            sr = nuke.toNode(rns)
+            if sr != None:
+                read_count += 1
+        if len(self.read_data) == read_count:
+            return True
+        else:
+            return False
+            
 
 '''
 #Add to Menu.py
